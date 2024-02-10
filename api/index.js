369c0c68d82f7dc3588ce5624ea4420f37bb1b11
harshitha0422 <31062202+harshitha0422@users.noreply.github.com>
2024-02-10 18:03:03 -0500

    Api route

--- api/index.js
+++ api/index.js
@@ -1,17 +1,20 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
 dotenv.config();
 
-mongoose
-  .connect(process.env.MONGO)
-  .then(() => {
-    console.log('Connected to MongoDB!');
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// mongoose
+//   .connect(process.env.MONGO)
+//   .then(() => {
+//     console.log('Connected to MongoDB!');
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
 const app = express();
-app.listen(3002, ()=>{
+app.listen(3005, ()=>{
+    console.log('Server is running on port 3005!');
 }
 );
+app.use('/api/user', userRouter);
