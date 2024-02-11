06bd0de537b7494c26f1ae14fa8a339d51bdac43
harshitha0422 <31062202+harshitha0422@users.noreply.github.com>
2024-02-11 16:36:43 -0500

    Middleware
    The middleware and sign up page functionality

--- api/index.js
+++ api/index.js
@@ -5,14 +5,14 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 dotenv.config();
 
-// mongoose
-//   .connect(process.env.MONGO)
-//   .then(() => {
-//     console.log('Connected to MongoDB!');
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 app.listen(3006, ()=>{
     console.log('Server is running on port 3005!');
@@ -21,3 +21,12 @@ app.listen(3006, ()=>{
 app.use(express.json());
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+      success: false,
+      statusCode,
+      message,
+    });
+  });
